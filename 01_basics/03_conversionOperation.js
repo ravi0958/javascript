1e8d660bf8d617bc9c09d7364efcf32bebfbe445
ravi0958 <2024aspire241@gmail.com>
2025-10-07 09:04:33 +0530

    conversion of data types

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,19 @@
+let score=true
+console.log(typeof (score))
+
+let value=Number(score)
+console.log(typeof(value))
+console.log(value)
+
+let isLoggedIn=1
+let booleanIsLoggedIn=Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn)
+
+//1=>true;  0=>false
+//""=>false
+//"ravi" =>true
+
+let someNumber=33
+let stringNumber=String(someNumber)
+console.log(stringNumber)
+console.log(typeof(stringNumber))
