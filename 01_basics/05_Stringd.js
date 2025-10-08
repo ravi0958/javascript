7a1fcaf3de99ec568894d8639c17eb75f1d52bf1
ravi0958 <2024aspire241@gmail.com>
2025-10-08 20:32:06 +0530

    strings

---
+++ 01_basics/05_Stringd.js
@@ -0,0 +1,30 @@
+//string->denote(' '," ")
+const name="Ravi Kumar"
+const repoCount=50
+//console.log(name + repoCount)
+
+//console.log(`Hello my name is ${name} and my repo count is ${repoCount}`)
+
+const gameName=new String("Robin-vats")
+// console.log(gameName[0])
+// console.log(gameName.__proto__)
+// console.log(gameName.length)
+// console.log(gameName.toLowerCase())
+// console.log(gameName.toUpperCase())
+// console.log(gameName.charAt(2))
+// console.log(gameName.indexOf('b'))
+console.log(gameName.split('-'))
+const newString=gameName.substring(0,4)
+// console.log(newString);
+
+const anotherString=gameName.slice(-8,4)
+// console.log(anotherString)
+
+const newStringOne="     Ravi     "
+// console.log(newStringOne)
+// console.log(newStringOne.trim())
+
+const url="https//hitesh.com/hitesh%20choudhary"
+// console.log(url.replace("%20", "-"))
+// console.log(url.includes('sunder'))
+
