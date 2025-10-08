1d888413dcdd5aa8b63db012eaf213c7c6c6f36e
ravi0958 <2024aspire241@gmail.com>
2025-10-08 11:21:55 +0530

    data rypes summary

---
+++ 01_basics/datatypes_summary.js
@@ -0,0 +1,45 @@
+//data types----> 2 types
+//Primitive 
+//7Types-> String,Number,Boolean,null,undefined,Symbol,BigInt
+
+const score=100
+const ScoreValue=100.3
+
+const isLoggedIn=false
+const outsideTemp=null
+let userEmail
+
+//Symbol
+
+const id=Symbol('234')
+const anotherId=Symbol('234')
+//console.log(id===anotherId)
+
+//BigInt
+
+const bigNumber=963247658789553244n
+//console.log(bigNumber);
+
+
+//Reference(Non Primitive)
+//Array,Objects,Functions
+
+
+//Arrays
+const heros=["shaktiman","naagraj","doga"]
+//console.log(heros);
+
+//Objects
+let myObj={
+    name:"Hitesh",
+    age:22
+}
+//console.log(myObj);
+
+//Functions
+const myFunctions=function(){
+    console.log("hello world");
+}
+
+
+
