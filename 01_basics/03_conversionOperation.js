7c410a6a81e8ee8e92aaaeb6665fad59c2c98f22
ravi0958 <2024aspire241@gmail.com>
2025-10-08 10:21:16 +0530

    conversion

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -1,4 +1,4 @@
-let score=true
+/*let score=true
 console.log(typeof (score))
 
 let value=Number(score)
@@ -17,3 +17,55 @@ let someNumber=33
 let stringNumber=String(someNumber)
 console.log(stringNumber)
 console.log(typeof(stringNumber))
+*/
+
+//**************** Operation ****************
+
+let value=4
+let negValue=-value
+//console.log(negValue)
+ 
+// console.log(2+2)
+// console.log(2-2)
+// console.log(2*2)
+// console.log(2/2)
+// console.log(3%2)
+// console.log(2**2)
+
+let str1="Hello"
+let str2=" Ravi"
+let str3=str1+str2
+//console.log(str3)
+
+// console.log(1+"2")
+// console.log("1"+2)
+// console.log("1"+"2")
+// console.log(1+2+"2")
+// console.log("1"+2+3)
+
+//console.log((3+4)*5/2)
+
+//console.log(+true)
+//console.log(+"")
+
+let num1,num2,num3
+num1=num2=num3=2+2
+//console.log(num1)
+
+let gamecounter=100;
+let yy=++gamecounter;
+console.log(gamecounter)
+console.log(yy)
+
+
+let x = 3;
+const y = x++;
+
+console.log(`x:${x}, y:${y}`);
+// Expected output: "x:4, y:3"
+
+let a = 3;
+const b = ++a;
+
+console.log(`a:${a}, b:${b}`);
+// Expected output: "a:4, b:4"
