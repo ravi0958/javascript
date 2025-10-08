9e1048dc80a9200e3ee64f57b77e81ee7f15cca8
ravi0958 <2024aspire241@gmail.com>
2025-10-08 10:36:52 +0530

    comparision

---
+++ 01_basics/04_comparsion.js
@@ -0,0 +1,21 @@
+// console.log(2<5)
+// console.log(2>5)
+// console.log(2==5)
+// console.log(2!=5)
+// console.log(2<=5)
+// console.log(2>=5)
+
+// console.log("2"<5)
+// console.log("02"<5)
+
+// console.log(null<0)
+// console.log(null==0)
+// console.log(null>=0)
+
+// console.log(undefined<0)
+// console.log(undefined==0)
+// console.log(undefined>0)
+
+//===
+
+console.log("2"===2)
