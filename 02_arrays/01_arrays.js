cac2f74dad1a49f13b80ffb602c32e845764ac68
ravi0958 <2024aspire241@gmail.com>
2025-10-10 01:33:52 +0530

    arrays

---
+++ 02_arrays/01_arrays.js
@@ -0,0 +1,15 @@
+// array
+
+const myArr=[1,2,3,4,5]
+console.log(myArr[4])
+
+//methods
+myArr.push(6)
+myArr.push(0)
+myArr.pop()  //no arguments remove last elements
+myArr.unshift(12)
+myArr.unshift(25)
+myArr.shift()
+console.log(myArr);
+
+const myArr1=new Array(1,2,3)
