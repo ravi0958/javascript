d878a03fc3669ce644116b76db4aec1843b7544b
ravi0958 <2024aspire241@gmail.com>
2025-10-10 21:01:16 +0530

    slice and splice

--- 02_arrays/01_arrays.js
+++ 02_arrays/01_arrays.js
@@ -1,15 +1,25 @@
-// array
+// array -->reliable.
 
-const myArr=[1,2,3,4,5]
-console.log(myArr[4])
 
-//methods
-myArr.push(6)
-myArr.push(0)
-myArr.pop()  //no arguments remove last elements
-myArr.unshift(12)
-myArr.unshift(25)
-myArr.shift()
+const myArr=[0,1,2,3,4,5]
+// console.log(myArr[4])
+
+//Arrays  methods
+// myArr.push(6)
+// myArr.push(7)
+// myArr.pop()  //no arguments remove last elements
+// myArr.unshift(12)
+// myArr.unshift(25)
+// myArr.shift()
+// console.log(myArr.includes(19))
+// console.log(myArr.indexOf(9))
+// console.log(myArr.join())
 console.log(myArr);
 
-const myArr1=new Array(1,2,3)
+//slice or splice
+console.log(myArr.slice(1,3))
+console.log(myArr.splice(1,3))
+
+
+//2.type of array represent 
+// const myArr1=new Array(1,2,3)
