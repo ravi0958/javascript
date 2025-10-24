a18c6b9c1a3b0eb6c5c01d98f2c08fc9c9e49c5e
ravi0958 <2024aspire241@gmail.com>
2025-10-24 13:18:48 +0530

    iteration

---
+++ 05_itrations/two.js
@@ -0,0 +1,18 @@
+// let index=0
+// while (index<=10) {
+//     console.log(index);
+//     index=index+1
+// }
+
+// let myArray=['flash','superman','batman']
+// let arr=0
+// while (arr<myArray.length) {
+//     console.log(`value of ${myArray[arr]}`);
+//     arr=arr+1
+// }
+
+let score=1
+do {
+    console.log(score);
+    score++
+} while (score<=10);
